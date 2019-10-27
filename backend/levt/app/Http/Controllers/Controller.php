d291a830f1a2cf39a54d3c59ac4f43f4fb25e005
MarioZeller <mario.zeller98@gmx.at>
2019-10-27 13:11:53 +0100

    Skeleton eingebaut

--- backend/levt/app/Http/Controllers/Controller.php
+++ backend/levt/app/Http/Controllers/Controller.php
@@ -7,4 +7,8 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     //
+    public function loadTopPosts() {
+        //Abfrage Users mit Journeys und Images und Likes und Places und Countries joinen,
+        //dann TOP 100 vong Likes her
+    }
 }
