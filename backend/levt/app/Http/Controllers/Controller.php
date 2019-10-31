d59e5e9a7d63d79162b9b1cb7c2a5fdf02781b03
MarioZeller <mario.zeller98@gmx.at>
2019-10-31 15:08:11 +0100

    #71 nullable variablen in den Tables, #73 select statement richtig

--- backend/levt/app/Http/Controllers/Controller.php
+++ backend/levt/app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
@@ -11,6 +12,17 @@ class Controller extends BaseController
         //Abfrage Users mit Journeys und Images und Likes und Places und Countries joinen,
         //dann TOP 100 vong Bookmarks her
 
-        //$result = DB::table('journeys')->select('journeyID','journeyName','users.username','userImage','')
+        $result = DB::select('SELECT journeys.journeyID,journeys.journeyName as name,users.username,
+                    profileImage.src as userImgSrc, COUNT(bookmarks.bookmarkID) AS bookmarks, journeys.arrivalDate, journeys.departureDate,
+                    null as places,thumbnailImage.src as thumbnailSrc
+                    FROM journeys
+                    INNER JOIN users ON journeys._userID = users.userID
+                    INNER JOIN images AS profileImage ON users._profileImageID = profileImage.imageID
+                    INNER JOIN images AS thumbnailImage ON journeys._thumbnailID = thumbnailImage.imageID
+                    INNER JOIN bookmarks ON journeys.journeyID = bookmarks._journeyID
+                    GROUP BY journeys.journeyID
+                    ORDER BY bookmarks DESC;');
+
+        return json_encode($result, JSON_PRETTY_PRINT);
     }
 }
