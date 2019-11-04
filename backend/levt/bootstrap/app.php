61dae7d8c81181350d200286de4650ef2468e418
Sebastian Vieghofer <55848667+SebastianVieghofer@users.noreply.github.com>
2019-11-04 16:24:34 +0100

    Cors Error fix
    A new Class in App->Http->Middleware was created and a line in bootstrap was added

--- backend/levt/bootstrap/app.php
+++ backend/levt/bootstrap/app.php
@@ -69,6 +69,10 @@ $app->singleton(
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+ ]);
+
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
