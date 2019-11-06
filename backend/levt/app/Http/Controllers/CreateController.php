cfec380d71efa8a187eecd6bab7b2973f9afc94a
MarioZeller <mario.zeller98@gmx.at>
2019-11-06 18:36:21 +0100

    input from test branch as a comment into CreateController

--- backend/levt/app/Http/Controllers/CreateController.php
+++ backend/levt/app/Http/Controllers/CreateController.php
@@ -24,4 +24,36 @@ class CreateController extends BaseController
     public function insertImage() {
 
     }
+
+    /*
+    //Post Requests und Module work:
+
+    public function listUsers(){
+        return User::all();
+    }
+
+    public function register(Request $request){
+        //print_r($request->input());
+        $newUser = new User;
+        $newUser->username = $request->input('username');
+        $newUser->pwHash = Hash::make($request->input('pwHash'));
+        echo $newUser->save();
+    }
+
+    public function login(Request $request){
+        $users = User::all();
+
+        foreach($users as $u){
+
+            //echo $u->username.$request->input('username');
+
+            if($u->username == $request->input('username')) {
+                if(Hash::check($request->input('pwHash'),$u->pwHash)){
+                    return "login successful";
+                }
+            }
+        }
+        return "could not login. please try again";
+    }
+    */
 }
