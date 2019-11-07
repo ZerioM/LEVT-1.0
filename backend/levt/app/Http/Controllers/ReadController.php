e3672018cbfd2db3dca1c9240ea282642483c437
Sebastian Vieghofer <55848667+SebastianVieghofer@users.noreply.github.com>
2019-11-07 17:14:51 +0100

    #79 Select top 100 completed

--- backend/levt/app/Http/Controllers/ReadController.php
+++ backend/levt/app/Http/Controllers/ReadController.php
@@ -14,14 +14,20 @@ class ReadController extends BaseController
 
         $result = DB::select('SELECT journeys.journeyID,journeys.journeyName as name,users.username,
                     profileImage.src as userImgSrc, COUNT(bookmarks.bookmarkID) AS bookmarks, 
+                    seasons.seasonName as season, journeys.year, 
+                    null as duration, null as companionship, null as detail, null as totalCosts,
+                    null as activityCosts, null as accommodationgCosts, null as foodCosts,
+                    null as transportCosts, null as otherCosts,
                     null as places,thumbnailImage.src as thumbnailSrc
                     FROM journeys
                     INNER JOIN users ON journeys._userID = users.userID
                     INNER JOIN images AS profileImage ON users._profileImageID = profileImage.imageID
                     INNER JOIN images AS thumbnailImage ON journeys._thumbnailID = thumbnailImage.imageID
                     INNER JOIN bookmarks ON journeys.journeyID = bookmarks._journeyID
+                    INNER JOIN seasons on journeys._seasonID = seasons.seasonID
                     GROUP BY journeys.journeyID
-                    ORDER BY bookmarks DESC;');
+                    ORDER BY bookmarks DESC
+                    LIMIT 100;');
 
         //return '"journeys":{ '.json_encode($result, JSON_PRETTY_PRINT)." \n}";
         return '{"journeys": '.json_encode($result, JSON_PRETTY_PRINT)." \n}";
