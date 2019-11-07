f76dae3d0eb75a181ef228eb59eab1d3d0ecc89b
MarioZeller <mario.zeller98@gmx.at>
2019-11-07 12:33:37 +0100

    Erase Comments

--- frontend/src/app/Pages/tab1/tab1.page.ts
+++ frontend/src/app/Pages/tab1/tab1.page.ts
@@ -9,15 +9,6 @@ import { NavController, IonContent } from '@ionic/angular';
 })
 export class Tab1Page {
 
-//@ViewChild (Content) content: Content;
-
-/*function smoothScrollJS(elementIDtoScrollTo, timeInMilli){
-  [document.documentElement,document.body].animate({
-    scrollTop: document.getElementById(elementIDtoScrollTo).offset().top}, timeInMilli);
-}*/
-
-
-
   constructor(private data: DataService, private navCtrl:NavController) {
 
     this.loadJSON();
