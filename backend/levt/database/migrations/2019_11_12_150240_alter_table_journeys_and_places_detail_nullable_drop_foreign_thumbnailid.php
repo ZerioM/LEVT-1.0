9cfcba7a185ffeeee91d6f752cc1ab847b0d01c2
Sebastian Vieghofer <55848667+SebastianVieghofer@users.noreply.github.com>
2019-11-12 16:13:56 +0100

    #12 Migration File Journeys und Places -> detail und thumbnail nullable gemacht

---
+++ backend/levt/database/migrations/2019_11_12_150240_alter_table_journeys_and_places_detail_nullable_drop_foreign_thumbnailid.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableJourneysAndPlacesDetailNullableDropForeignThumbnailid extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('journeys', function($table) {
+            $table->string('detail')->nullable()->change();
+            $table->dropForeign('journeys__thumbnailID_foreign');
+        });
+
+        Schema::table('places', function($table) {
+            $table->dropForeign('places__thumbnailID_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
