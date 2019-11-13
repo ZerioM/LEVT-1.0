7037d7b2836f343bea9ba7dc4fff06796a20a207
SandraSzeliga <52004178+SandraSzeliga@users.noreply.github.com>
2019-11-13 18:59:44 +0100

    #17 #13 Ion Input Values auslesen

--- frontend/src/app/Pages/tab2/tab2.page.ts
+++ frontend/src/app/Pages/tab2/tab2.page.ts
@@ -3,6 +3,7 @@ import { DataService } from 'src/app/services/data.service';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { NewJourneyService } from 'src/app/services/new-journey.service';
+import { equal } from 'assert';
 
 @Component({
   selector: 'app-tab2',
@@ -14,6 +15,14 @@ export class Tab2Page {
   showC:Boolean = false;
   divideC:Boolean = false;
   noPlace:Boolean=true;
+
+  leisureCosts:number;
+  accommondationCosts:number;
+  mealsanddrinksCosts:number;
+  transportCosts:number;
+  otherCosts:number;
+  totalCosts:number;
+
 
   constructor(private data: NewJourneyService, private navCtrl:NavController,  private router: Router) {
     this.loadJSON();
@@ -49,9 +58,47 @@ export class Tab2Page {
 
   }
 
-  calculateTotalCosts(){
+  safeJourney(){
+    //Hier kommt die Implementierung des Speicherns der Journey 
 
+    let inputs = document.querySelectorAll('ion-input');
+    let i = 0;
+    let a="";
     
+      inputs.forEach(input => {
+
+      if(input.id.toString() == "leisure"){
+        this.leisureCosts=parseInt(input.value);
+        console.log("Leisure Costs ="+this.leisureCosts);
+      }
+      if(input.id.toString() == "accommodation"){
+        this.accommondationCosts=parseInt(input.value);
+        console.log("AccomndationCosts ="+this.accommondationCosts);
+      }
+
+      if(input.id.toString() == "mealsanddrinks"){
+        this.mealsanddrinksCosts=parseInt(input.value);
+        console.log("Meals and Drink Costs ="+this.mealsanddrinksCosts);
+      }
+
+      if(input.id.toString() == "transport"){
+        this.transportCosts=parseInt(input.value);
+        console.log("Transport Costs ="+this.transportCosts);
+      }
+
+      if(input.id.toString() == "other"){
+        this.otherCosts=parseInt(input.value);
+        console.log("OtherCosts ="+this.otherCosts);
+      }
+
+
+    });
+
+    this.totalCosts=this.leisureCosts+this.mealsanddrinksCosts+this.otherCosts+this.transportCosts+this.accommondationCosts;
+    console.log("TotalCosts:"+ this.totalCosts)
+    this.router.navigateByUrl('/tabs/tab1');
   }
+
+  
 }
 
