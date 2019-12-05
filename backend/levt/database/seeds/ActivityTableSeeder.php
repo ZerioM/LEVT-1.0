ebaab9f33a7c61e05d24f5a4443c4603244f307d
sarahneko97 <sarah.gartner97@gmail.com>
2019-12-05 15:27:41 +0100

    #11 icons zu activities
    Co-Authored-By: sebastianvieghofer <sebastianvieghofer@users.noreply.github.com>

--- backend/levt/database/seeds/ActivityTableSeeder.php
+++ backend/levt/database/seeds/ActivityTableSeeder.php
@@ -14,18 +14,23 @@ class ActivityTableSeeder extends Seeder
         //
         DB::table('activities')->insert([
             'activityName' => 'Leisure',
+            'iconName' => 'md-football'
         ]);
         DB::table('activities')->insert([
             'activityName' => 'Accommodation',
+            'iconName' => 'md-bed'
         ]);
         DB::table('activities')->insert([
             'activityName' => 'Meals and Drinks',
+            'iconName' => 'ios-restaurant'
         ]);
         DB::table('activities')->insert([
             'activityName' => 'Transport',
+            'iconName' => 'md-airplane'
         ]);
         DB::table('activities')->insert([
             'activityName' => 'Other',
+            'iconName' => 'ios-more'
         ]);
 
     }
