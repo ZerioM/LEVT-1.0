6999ef3534300e5db4fa3747d06504ebaca3501a
SandraSzeliga <52004178+SandraSzeliga@users.noreply.github.com>
2019-12-18 18:12:00 +0100

    #121 register und login view erstellt

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: 'add-place', loadChildren: './Pages/add-place/add-place.module#AddPlacePageModule' },  { path: 'add-post', loadChildren: './Pages/add-post/add-post.module#AddPostPageModule' },
+  { path: 'add-place', loadChildren: './Pages/add-place/add-place.module#AddPlacePageModule' },
+  { path: 'add-post', loadChildren: './Pages/add-post/add-post.module#AddPostPageModule' },
   { path: 'journey-detail', loadChildren: './Pages/journey-detail/journey-detail.module#JourneyDetailPageModule' },
   { path: 'place-detail', loadChildren: './Pages/place-detail/place-detail.module#PlaceDetailPageModule' },
   { path: 'post-detail', loadChildren: './Pages/post-detail/post-detail.module#PostDetailPageModule' },
   { path: 'user-journey-detail', loadChildren: './Pages/user-journey-detail/user-journey-detail.module#UserJourneyDetailPageModule' },
   { path: 'user-place-detail', loadChildren: './Pages/user-place-detail/user-place-detail.module#UserPlaceDetailPageModule' },
   { path: 'user-post-detail', loadChildren: './Pages/user-post-detail/user-post-detail.module#UserPostDetailPageModule' },
+  
 
 
  
