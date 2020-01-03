9cd12558b39a602ca13e060373f6c48cd8f74dd3
SandraSzeliga <52004178+SandraSzeliga@users.noreply.github.com>
2020-01-03 16:33:33 +0100

    #252 Login Button Settings Page

--- frontend/src/app/Pages/settings/settings.page.ts
+++ frontend/src/app/Pages/settings/settings.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class SettingsPage implements OnInit {
 
-  constructor(private data: DataService, private navCtrl:NavController, private router: Router) { }
+  constructor(private data: DataService, private navCtrl:NavController, private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -27,5 +27,67 @@ export class SettingsPage implements OnInit {
 
 
    }
+
+   showLogoutAlert(){
+
+    this.alert();
+   }
+
+   showLogin(){
+
+    this.data.wantsToLogin=true;
+    
+  }
+
+  cancleWantsToLogin(){
+
+    this.data.wantsToLogin=false;
+  }
+
+  cancleWantsToRegister(){
+
+    this.data.wantsToRegister=false;
+  }
+
+   async alert() {
+    const alert = await this.alertController.create({
+      header: 'Are you sure you want to log out?',
+      //message: '<strong>Would you like to save your created journey?</strong>',
+      buttons: [
+        {
+          text: 'Log Out',
+          handler: () => {
+            console.log('Logout Okal');
+            this.logout();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+            this.goBacktoSettingsWithoutSaving();
+          }
+        }, 
+      ]
+    });
+
+    await alert.present();
+  }
+
+  logout(){
+
+    this.data.userLoggedIn=false;
+  }
+
+  
+
+  goBacktoSettingsWithoutSaving(){
+
+    this.router.navigateByUrl('/tabs/tab1/settings');
+  }
+
+  
 
 }
