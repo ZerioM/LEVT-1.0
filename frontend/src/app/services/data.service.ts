97674f67fcf6f5ecfec2c2c846279cccdcf01584
MarioZeller <mario.zeller98@gmx.at>
2020-01-07 17:07:23 +0100

    bugfixes

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -144,7 +144,7 @@ export class DataService {
 
   public flock: string = "https://flock-1427.students.fhstp.ac.at/backend/public";
   public homestead: string = "http://levt.test";
-  public url: string = this.flock;
+  public url: string = this.homestead;
 
   constructor(private http: HttpClient, private journeyService: NewJourneyService, private placeService: PlaceService, private postService: PostService,private imageService:ImageService, public toastController: ToastController, public loadingController:LoadingController) { 
 
