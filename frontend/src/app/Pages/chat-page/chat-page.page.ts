84d1121302f77b3e41d34716f9828c0b5e0950bd
MarioZeller <mario.zeller98@gmx.at>
2020-01-21 17:06:05 +0100

    Bugfix: Chat

--- frontend/src/app/Pages/chat-page/chat-page.page.ts
+++ frontend/src/app/Pages/chat-page/chat-page.page.ts
@@ -10,15 +10,15 @@ export class ChatPagePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  doRefresh(event) {
-    console.log('Begin async operation');
+  // doRefresh(event) {
+  //   console.log('Begin async operation');
     
 
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      event.target.complete();
-      this.messagesService.loadMessages(this.data.currentMessages, this.data.url, this.data.loggedInUser, this.data.chatUser);
-    }, 500);
+  //   setTimeout(() => {
+  //     console.log('Async operation has ended');
+  //     event.target.complete();
+  //     this.messagesService.loadMessages(this.data.currentMessages, this.data.url, this.data.loggedInUser, this.data.chatUser);
+  //   }, 500);
   }
 
 }
